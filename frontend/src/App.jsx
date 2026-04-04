f33963c3103b07d824ee0a4bf7c80d64f33c8ba3
Toshiaki Tominari <tominarit@gmail.com>
2026-04-04 13:10:03 +0900

    added home and create page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
@@ -19,9 +19,9 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/product/:id" element={<ProductPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/create" element={<CreatePage />} />
-          <Route path="/edit/:id" element={<EditProductPage />} />
+          <Route path="/profile" element={isSignedIn ? <ProfilePage /> : <Navigate to={"/"} />} />
+          <Route path="/create" element={isSignedIn ? <CreatePage /> : <Navigate to={"/"} />} />
+          <Route path="/edit/:id" element={isSignedIn ? <EditProductPage /> : <Navigate to={"/"} />} />
         </Routes>
       </main>
     </div>
